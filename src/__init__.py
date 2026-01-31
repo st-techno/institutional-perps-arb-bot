ca44b42c3efaff0f2f74d323881d88f6b1fb975d
ST Technologies <98237904+st-techno@users.noreply.github.com>
2026-01-31 19:16:22 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Institutional Perps Arbitrage Bot - Production Entry Point
+Dynamic event discovery + Morpho vaults + 24/7 human oversight
+"""
+__version__ = "2.1.0"
+__author__ = "ST Technologies"
